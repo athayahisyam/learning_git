b25469a33c5cc4d2c98027974cb25bc52f483b47
Athveia_Hisham <hisyamathaya@unida.gontor.ac.id>
2019-05-05 21:29:38 +0700

    second commit

--- hello.php
+++ hello.php
@@ -3,5 +3,19 @@
 $halo = 'hello world';
 
 echo($hello);
+$no = 0;
 
 ?>
+
+<html>
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Teks</th>
+        </tr>
+        <tr>
+            <td><?= $no++; ?></td>
+            <td><?= $hello ?></td>
+        </tr>
+    </table>
+</html>
